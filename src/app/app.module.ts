acd0ac3ef05b352ba90f22540be469842b1bab8b
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2021-12-02 20:14:28 -0800

    week 6

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { SocialSharing } from '@ionic-native/social-sharing/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { GroceriesServiceService } from './groceries-service.service';
+import { InputDialogServiceService } from './input-dialog-service.service';
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
+    HttpClientModule,
+  ],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SocialSharing,
+    GroceriesServiceService,
+    InputDialogServiceService,
+  ],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
